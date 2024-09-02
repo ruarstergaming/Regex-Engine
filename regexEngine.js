56e67e9bbfe9905fb711b268b0374cc66130d41a
Marcy <ruarster@gmail.com>
2024-09-02 15:53:24 +0100

    Now searches whole string

--- regexEngine.js
+++ regexEngine.js
@@ -16,13 +16,31 @@ function matchOne(pattern, text){
  * checking each character matches the pattern
  * @param {*} pattern 
  * @param {*} text 
- * @returns 
+ * @returns boolean if the pattern and text match
  */
 function match(pattern, text){
     if(!pattern || pattern === "") return true; //Base case - if the pattern is empty any inputted text is a match
+    if(pattern === "$" && text === "") return true; //$ allows matching the end of a string, a simple other base case in implementation
     if(!text) return false;
     else {
         return (matchOne(pattern[0], text[0]) && match(pattern.slice(1), text.slice(1)));
     }
-
 }
+
+/**
+ * For now only allows ^ integration, forcing the user to preface patterns with ^
+ * @param {*} pattern 
+ * @param {*} text 
+ */
+function search(pattern, text){
+    if(pattern && pattern[0] ==="^") {
+        return match(pattern.slice(1), text);
+    }
+    else{
+        //This runs match on every index of the text, meaning the pattern is tested against every starting point of text
+        return text.split("").some((_, index) => {
+            return match(pattern, text.slice(index));
+        });
+    }
+}
+
